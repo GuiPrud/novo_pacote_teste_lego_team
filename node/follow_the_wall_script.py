9440605c172342b9baf48c6a640d398112c57aee
GuilhermePrudente197 <gprudente@gmail.com>
2020-11-23 23:16:47 -0300

    O controle de posicao esta feito, agora eh necessario implementar o controle de velocidade

--- node/follow_the_wall_script.py
+++ node/follow_the_wall_script.py
@@ -4,28 +4,76 @@ import rospy
 import time
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Quaternion
 import math
 
+orientacao_objetivo = Quaternion(x=0,y=0,z=0,w=0)
+orientacao_atual = Quaternion(x=0,y=0,z=0,w=0)
+posicao_objetivo = Point(x = 5, y = 5)
+posicao_atual = Point(0,0,0);
+
+
+posicao = 1
+
 def callback(msg):
-	bolota = 1
+	global orientacao_atual
+	global posicao_atual
+	orientacao_atual = msg.pose.pose.orientation
+	posicao_atual = msg.pose.pose.position
 
-class FollowTheGap(object):
 
-    
+class FollowTheGap(object):    
 
-    def __init__(self):
-
-        #rospy.init_node('new_lego_team_node', anonymous = False)
-
-        #self.sub = rospy.Subscriber('/scan', LaserScan, callback)
-	#self.sub = rospy.Subscriber('/new_lego_team_id/scan', LaserScan, callback)
+    def __init__(self):	
+	
+	#rospy.loginfo(str(objetivo.x))
+	rate = rospy.Rate(40)
 	direc_msg = AckermannDriveStamped()
-        rate = rospy.Rate(40)
-
         while not rospy.is_shutdown():
 
-		direc_msg.drive.speed = 0.1
-		direc_msg.drive.steering_angle = 0
+		global orientacao_objetivo
+		global orientacao_atual
+		global posicao_objetivo
+
+		angulo_atual = 2*math.asin(orientacao_atual.z)			
+		
+		# Essas condições servem para que os angulos fiquem entre 0<theta<180 e 0>theta>-180
+
+		if(posicao_atual.x < posicao_objetivo.x):
+			angulo_objetivo = math.atan((posicao_objetivo.y - posicao_atual.y)/(posicao_objetivo.x - posicao_atual.x))				
+		else:	
+			if (posicao_atual.y < posicao_objetivo.y):
+				angulo_objetivo = math.pi+math.atan((posicao_objetivo.y - posicao_atual.y)/(posicao_objetivo.x - posicao_atual.x))	
+			else:
+				angulo_objetivo = -math.pi+math.atan((posicao_objetivo.y - posicao_atual.y)/(posicao_objetivo.x - posicao_atual.x))	
+		
+		#rospy.loginfo(str(angulo_objetivo*180/math.pi))
+		erro = angulo_objetivo -angulo_atual
+
+		Kerro = -1	
+		K = 1
+
+		distancia_objetivo = math.sqrt(math.pow((posicao_objetivo.y - posicao_atual.y),2) + math.pow((posicao_objetivo.x - posicao_atual.x),2))
+
+		#velocidade = Kerro*abs(erro)+K*distancia_objetivo/27
+		global posicao
+		velocidade = 4
+		if(distancia_objetivo < 0.5):
+			if(posicao == 1):
+				posicao_objetivo = Point(x = 15, y = 9)
+				posicao = 2
+			elif(posicao == 2):
+				posicao_objetivo = Point(x = 18, y = 5)
+				posicao = 3
+			elif(posicao == 3):
+				posicao_objetivo = Point(x = 13, y = 0)
+				posicao = 1
+
+		direc_msg.drive.speed = velocidade
+		direc_msg.drive.steering_angle = erro
+
 		gap_pub.publish(direc_msg)
 		rate.sleep()
 
@@ -34,12 +82,12 @@ def main():
 	rospy.init_node('new_lego_team_node', anonymous = False)
 
 	#self.sub = rospy.Subscriber('/scan', LaserScan, callback)
-	sub = rospy.Subscriber('/scan', LaserScan, callback)
+	sub = rospy.Subscriber('/odom', Odometry, callback)
 
 	fg = FollowTheGap()
 	rospy.spin()
 
-#gap_pub = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=10)
+
 gap_pub = rospy.Publisher('/drive', AckermannDriveStamped, queue_size=10)
 
 main()

c1cf6f8aae94e97a610ca763c6add66bbb619167
GuilhermePrudente197 <gprudente@gmail.com>
2020-11-25 23:14:01 -0300

    Foi adicionado um arquivo com a tragetoria do carro e o codigo ja esta lendo esse arqvui com a tragetoria

--- node/follow_the_wall_script.py
+++ node/follow_the_wall_script.py
@@ -8,14 +8,16 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Point
 from geometry_msgs.msg import Quaternion
 import math
+import os
 
 orientacao_objetivo = Quaternion(x=0,y=0,z=0,w=0)
 orientacao_atual = Quaternion(x=0,y=0,z=0,w=0)
 posicao_objetivo = Point(x = 5, y = 5)
 posicao_atual = Point(0,0,0);
 
-
 posicao = 1
+
+os.chdir(r'/home/guilherme/catkin_ws/src/novo_pacote_teste_lego_team/node')
 
 def callback(msg):
 	global orientacao_atual
@@ -23,6 +25,18 @@ def callback(msg):
 	orientacao_atual = msg.pose.pose.orientation
 	posicao_atual = msg.pose.pose.position
 
+#essa funcao abre um arquivo .csv e retorna um vetor com os dados presentes na linha passada como parametro
+def ler_csv(n_linha):
+	arquivo = open(r'pontos.csv', "r") #coloca o arquivo de pontos dentro do path do node, ou declara um path absoluto
+			              #tipo "/home/luizfpastuch/Documents/trajectory_map/pontos.csv" pra deixar em uma pasta diferente (acho que precisa)
+	linhas = arquivo.readlines() #le todas as linhas do arquivo
+	linha = linhas[n_linha-1] #pega a linha que quer
+	vetor_string = linha.split(',') #divide a linha a partir das virgulas (achei que seria mais dificil kk)
+	vetor_float = [] #declara o novo vetor pra retornar
+	for i in range(len(vetor_string)): #converte cada item do vetor de string pra float
+		vetor_float.append(float(vetor_string[i]))
+	arquivo.close() #fecha o arquivo
+	return vetor_float[0:2] #retorna o novo vetor
 
 class FollowTheGap(object):    
 
@@ -60,16 +74,23 @@ class FollowTheGap(object):
 		#velocidade = Kerro*abs(erro)+K*distancia_objetivo/27
 		global posicao
 		velocidade = 4
+
 		if(distancia_objetivo < 0.5):
-			if(posicao == 1):
-				posicao_objetivo = Point(x = 15, y = 9)
-				posicao = 2
-			elif(posicao == 2):
-				posicao_objetivo = Point(x = 18, y = 5)
-				posicao = 3
-			elif(posicao == 3):
-				posicao_objetivo = Point(x = 13, y = 0)
-				posicao = 1
+			novo_objetivo = ler_csv(posicao)
+			posicao = posicao + 1
+			posicao_objetivo = Point(x = novo_objetivo(1), y = novo_objetivo(2))
+
+		
+		#if(distancia_objetivo < 0.5):
+		#	if(posicao == 1):
+		#		posicao_objetivo = Point(x = 15, y = 9)
+		#		posicao = 2
+		#	elif(posicao == 2):
+		#		posicao_objetivo = Point(x = 18, y = 5)
+		#		posicao = 3
+		#	elif(posicao == 3):
+		#		posicao_objetivo = Point(x = 13, y = 0)
+		#		posicao = 1
 
 		direc_msg.drive.speed = velocidade
 		direc_msg.drive.steering_angle = erro
